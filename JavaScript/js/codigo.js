400188832bcfc93f1f7671875f2e9d141471d821
AbrahamG1995 <jesus.abraham1995@outlook.com>
2016-04-12 10:59:34 -0600

    Archivo JS, funcion anonima y consola

---
+++ JavaScript/js/codigo.js
@@ -0,0 +1,9 @@
+var botonClick = function()
+{
+	alert("Click del botón");
+}
+
+var nombre = "Martin";
+document.write("<strong>JS en acción</strong>"+" "+nombre);
+console.log("A punto de lanzar un mensaje");
+alert("Mensaje por pantalla");
